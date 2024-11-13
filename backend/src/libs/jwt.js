b4d1909e5cd9066324d721869d5ab03297192497
LeandroMQ2018 <deinermamani34@gmail.com>
2024-11-13 07:45:20 -0400

    Delete backend directory

--- backend/src/libs/jwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken"; 
-import { config } from "../config.js"; 
-
-// Función para crear un token de acceso
-export const createAccessToken = (payload) => {
-  // Genera y devuelve un token firmado usando el payload proporcionado
-  return jwt.sign(payload, config.jwtSecret, { expiresIn: "1d" }); // El token expirará en 1 día
-};
